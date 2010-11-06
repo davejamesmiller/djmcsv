2e70bd7a644b43e40934a717ad4b0679607c5839
Dave Miller <mercurial@dave-miller.com>
2010-11-06 19:22:01 +0000

    Switch to 4 space indents, Zend-style indenting

--- classes/djmCsv.php
+++ classes/djmCsv.php
@@ -26,113 +26,95 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 class djmCsv
 {
-  
-  public static function headers($options = array())
-  {
-    $options = array_merge(array(
-      'filename' => null,
-    ), $options);
     
-    header('Content-Type: text/csv');
-    ini_set('html_errors', false);
-    
-    if ($options['filename'])
+    public static function headers($options = array())
     {
-      header('Content-Disposition: attachment; filename=' . $options['filename']);
-    }
-  }
-  
-  public static function generateRow($row, $options = array())
-  {
-    $options = array_merge(array(
-      'delimiter' => ',',
-      'enclosure' => '"',
-      'escape'    => '"',
-      'excel'     => false,
-    ), $options);
-    
-    $output = '';
-    $firstCol = true;
-    
-    foreach ($row as $col)
-    {
-      if ($firstCol)
-      {
-        $firstCol = false;
-      }
-      else
-      {
-        $output .= $options['delimiter'];
-      }
-      
-      if ($options['excel'])
-      {
-        if (is_string($col) && is_numeric($col))
-        {
-          // Excel hack to display phone numbers correctly!
-          // http://www.creativyst.com/Doc/Articles/CSV/CSV01.htm
-          $output .= '=';
+        $options = array_merge(array(
+            'filename' => null,
+        ), $options);
+        
+        header('Content-Type: text/csv');
+        ini_set('html_errors', false);
+        
+        if ($options['filename']) {
+            header('Content-Disposition: attachment; filename=' . $options['filename']);
         }
-      }
-      
-      $output .= $options['enclosure']
-              .  str_replace($options['enclosure'], $options['escape'] . $options['enclosure'], $col)
-              .  $options['enclosure'];
     }
     
-    return $output;
-  }
-  
-  public static function generate($data, $options = array())
-  {
-    $options = array_merge(array(
-      'headings' => false,
-    ), $options);
-    
-    $output = '';
-    $firstRow = true;
-    
-    foreach ($data as $row)
+    public static function generateRow($row, $options = array())
     {
-      if ($firstRow)
-      {
-        if ($options['headings'])
-        {
-          if (is_array($options['headings']))
-          {
-            $headings = $options['headings'];
-          }
-          elseif (is_object($row) && method_exists($row, 'toArray'))
-          {
-            $headings = array_keys($row->toArray());
-          }
-          else
-          {
-            $headings = (array) array_keys($row);
-          }
-          $output .= self::generateRow($headings, $options);
-          $output .= "\n";
+        $options = array_merge(array(
+            'delimiter' => ',',
+            'enclosure' => '"',
+            'escape'    => '"',
+            'excel'     => false,
+        ), $options);
+        
+        $output = '';
+        $firstCol = true;
+        
+        foreach ($row as $col) {
+            if ($firstCol) {
+                $firstCol = false;
+            } else {
+                $output .= $options['delimiter'];
+            }
+            
+            if ($options['excel']) {
+                if (is_string($col) && is_numeric($col)) {
+                    // Excel hack to display phone numbers correctly!
+                    // http://www.creativyst.com/Doc/Articles/CSV/CSV01.htm
+                    $output .= '=';
+                }
+            }
+            
+            $output .= $options['enclosure']
+                    .  str_replace($options['enclosure'], $options['escape'] . $options['enclosure'], $col)
+                    .  $options['enclosure'];
         }
-        $firstRow = false;
-      }
-      else
-      {
-        $output .= "\n";
-      }
-      
-      $output .= self::generateRow($row, $options);
+        
+        return $output;
     }
     
-    return $output;
-  }
-  
-  public static function output($data, $options = array())
-  {
-    // Generate first so any exceptions are shown in the browser not in the file
-    $data = self::generate($data, $options);
-    self::headers($options);
-    echo $data;
-  }
-  
+    public static function generate($data, $options = array())
+    {
+        $options = array_merge(array(
+            'headings' => false,
+        ), $options);
+        
+        $output = '';
+        $firstRow = true;
+        
+        foreach ($data as $row) {
+            if ($firstRow) {
+                if ($options['headings']) {
+                    if (is_array($options['headings'])) {
+                        $headings = $options['headings'];
+                    } elseif (is_object($row) && method_exists($row, 'toArray')) {
+                        $headings = array_keys($row->toArray());
+                    } else {
+                        $headings = (array) array_keys($row);
+                    }
+                    $output .= self::generateRow($headings, $options);
+                    $output .= "\n";
+                }
+                $firstRow = false;
+            } else {
+                $output .= "\n";
+            }
+            
+            $output .= self::generateRow($row, $options);
+        }
+        
+        return $output;
+    }
+    
+    public static function output($data, $options = array())
+    {
+        // Generate first so any exceptions are shown in the browser not in the file
+        $data = self::generate($data, $options);
+        self::headers($options);
+        echo $data;
+    }
+    
 }
-
